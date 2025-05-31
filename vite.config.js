5bf5c6dfa2763d318c3e5c693bbc1d8d5e6cae8b
aadil <aadilkhan085529@gmail.com>
2025-05-31 22:04:56 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/centralized_ai/', // Ensure correct asset paths for GitHub Pages
   server: {
     proxy: {
-      '/api': 'https://aadil.pythonanywhere.com',
+      '/api': 'http://localhost:4000',
     },
   },
 })
